f8c9044f74afba1b82aa3164a6121799a379ce3c
Luisana <163352714+LuisanaBMG@users.noreply.github.com>
2025-01-08 14:53:23 -0400

    Add files via upload

--- JS/Modal.js
+++ JS/Modal.js
@@ -1,17 +1,17 @@
 // Wait for the DOM content to be fully loaded before executing
 document.addEventListener('DOMContentLoaded', function () {
-    // Handle view button click for studies
+    // Handle view button click 
     document.querySelectorAll('.view-btn').forEach(button => {
         button.addEventListener('click', function () {
             var row = this.closest('tr');
             var idStudy = row.cells[0].textContent;
-            var diminutive = row.cells[1].textContent;
+            var diminutiveType = row.cells[1].textContent;
             var studyName = row.cells[2].textContent;
             var studyDate = row.cells[3].textContent;
 
             // Fill form fields with data from clicked row
             document.getElementById('Id_Study_Types').value = idStudy;
-            document.getElementById('Diminutive_Study').value = diminutive;
+            document.getElementById('Diminutive_Study').value = diminutiveType ;
             document.getElementById('Study_Type').value = studyName;
             document.getElementById('Date_Study').value = studyDate;
 
@@ -24,18 +24,18 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 
-    // Handle edit button click for studies
+    // Handle edit button click 
     document.querySelectorAll('.edit-btn').forEach(button => {
         button.addEventListener('click', function () {
             var row = this.closest('tr');
             var idStudy = row.cells[0].textContent;
-            var diminutive = row.cells[1].textContent;
+            var diminutiveType  = row.cells[1].textContent;
             var studyName = row.cells[2].textContent;
             var studyDate = row.cells[3].textContent;
 
             // Fill form fields with data from clicked row
             document.getElementById('Id_Study_Types').value = idStudy;
-            document.getElementById('Diminutive_Study').value = diminutive;
+            document.getElementById('Diminutive_Study').value = diminutiveType ;
             document.getElementById('Study_Type').value = studyName;
             document.getElementById('Date_Study').value = studyDate;
 
@@ -50,6 +50,7 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 document.addEventListener('DOMContentLoaded', function () {
+    // Handle view button click for studies
     document.querySelectorAll('.view-btn').forEach(button => {
         button.addEventListener('click', function () {
             var row = this.closest('tr');
@@ -73,6 +74,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     document.querySelectorAll('.edit-btn').forEach(button => {
+        // Handle edit button click 
         button.addEventListener('click', function () {
             var row = this.closest('tr');
             var Id_Units = row.cells[0].textContent;
@@ -96,6 +98,7 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 document.addEventListener('DOMContentLoaded', function () {
+    // Handle view button click for studies
     document.querySelectorAll('.view-btn').forEach(button => {
         button.addEventListener('click', function () {
             var row = this.closest('tr');
@@ -118,6 +121,7 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
     document.querySelectorAll('.edit-btn').forEach(button => {
+        // Handle edit button click 
         button.addEventListener('click', function () {
             var row = this.closest('tr');
             var Id_Academy = row.cells[0].textContent;
@@ -141,6 +145,7 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 document.addEventListener('DOMContentLoaded', function () {
+    // Handle view button click for studies
     document.querySelectorAll('.view-btn').forEach(button => {
         button.addEventListener('click', function () {
             var row = this.closest('tr');
@@ -164,6 +169,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     document.querySelectorAll('.edit-btn').forEach(button => {
+        // Handle edit button click 
         button.addEventListener('click', function () {
             var row = this.closest('tr');
             var Id_Resources = row.cells[0].textContent;
@@ -187,6 +193,7 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 document.addEventListener('DOMContentLoaded', function () {
+    // Handle view button click for studies
     document.querySelectorAll('.view-btn').forEach(button => {
         button.addEventListener('click', function () {
             var row = this.closest('tr');
@@ -209,6 +216,7 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
     document.querySelectorAll('.edit-btn').forEach(button => {
+        // Handle edit button click 
         button.addEventListener('click', function () {
             var row = this.closest('tr');
             var Id_Associate = row.cells[0].textContent;
@@ -232,6 +240,7 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 document.addEventListener('DOMContentLoaded', function () {
+    // Handle view button click for studies
     document.querySelectorAll('.view-btn').forEach(button => {
         button.addEventListener('click', function () {
             var row = this.closest('tr');
@@ -239,7 +248,7 @@ document.addEventListener('DOMContentLoaded', function () {
             var Document_Type = row.cells[1].textContent;
             var Identification_Document = row.cells[2].textContent;
             var Date_Birth = row.cells[4].textContent;
-            var Firts_Name = row.cells[5].textContent;
+            var First_Name = row.cells[5].textContent;
             var Second_Name = row.cells[6].textContent;
             var First_LastName = row.cells[7].textContent;
             var Second_LastName = row.cells[8].textContent;
@@ -253,7 +262,7 @@ document.addEventListener('DOMContentLoaded', function () {
             document.getElementById('Document_Type').value = Document_Type;
             document.getElementById('Identification_Document').value = Identification_Document;
             document.getElementById('Date_Birth').value = Date_Birth;
-            document.getElementById('Firts_Name').value = Firts_Name;
+            document.getElementById('First_Name').value = First_Name;
             document.getElementById('Second_Name').value = Second_Name;
             document.getElementById('First_LastName').value = First_LastName;
             document.getElementById('Second_LastName').value = Second_LastName;
@@ -273,13 +282,14 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     document.querySelectorAll('.edit-btn').forEach(button => {
+        // Handle edit button click 
         button.addEventListener('click', function () {
             var row = this.closest('tr');
             var Id_Responsible = row.cells[0].textContent;
             var Document_Type = row.cells[1].textContent;
             var Identification_Document = row.cells[2].textContent;
             var Date_Birth = row.cells[4].textContent;
-            var Firts_Name = row.cells[5].textContent;
+            var First_Name = row.cells[5].textContent;
             var Second_Name = row.cells[6].textContent;
             var First_LastName = row.cells[7].textContent;
             var Second_LastName = row.cells[8].textContent;
@@ -293,7 +303,7 @@ document.addEventListener('DOMContentLoaded', function () {
             document.getElementById('Document_Type').value = Document_Type;
             document.getElementById('Identification_Document').value = Identification_Document;
             document.getElementById('Date_Birth').value = Date_Birth;
-            document.getElementById('Firts_Name').value = Firts_Name;
+            document.getElementById('First_Name').value = First_Name;
             document.getElementById('Second_Name').value = Second_Name;
             document.getElementById('First_LastName').value = First_LastName;
             document.getElementById('Second_LastName').value = Second_LastName;
@@ -314,6 +324,7 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 document.addEventListener('DOMContentLoaded', function () {
+    // Handle view button click for studies
     document.querySelectorAll('.view-btn').forEach(button => {
         button.addEventListener('click', function () {
             var row = this.closest('tr');
@@ -338,6 +349,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     document.querySelectorAll('.edit-btn').forEach(button => {
+        // Handle edit button click 
         button.addEventListener('click', function () {
             var row = this.closest('tr');
             var Id_RA = row.cells[0].textContent;
@@ -364,7 +376,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 document.addEventListener('DOMContentLoaded', function () {
-
+    // Handle view button click for studies
     document.querySelectorAll('.view-btn').forEach(button => {
         button.addEventListener('click', function () {
             var row = this.closest('tr');
@@ -423,6 +435,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
     document.querySelectorAll('.edit-btn').forEach(button => {
+        // Handle edit button click 
         button.addEventListener('click', function () {
             var row = this.closest('tr');
             var Id_Studies = row.cells[0].textContent;
@@ -446,7 +459,7 @@ document.addEventListener('DOMContentLoaded', function () {
             document.getElementById('Id_Studies').value = Id_Studies;
             document.getElementById('Study_Code').value = Study_Code;
             document.getElementById('Study_Name').value = Study_Name;
-            document.getElementById('Cohort').value = Cohort
+            document.getElementById('Cohort').value = Cohort;
             document.getElementById('Year').value = Year;
             document.getElementById('Identification_Document').value = Identification_Document;
             document.getElementById('Number_Hours').value = Number_Hours;
@@ -481,3 +494,128 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+    // Handle view button click for studies
+    document.querySelectorAll('.view-btn').forEach(button => {
+        button.addEventListener('click', function () {
+            var row = this.closest('tr');
+            var Id_Student = row.cells[0].textContent;
+            var Document_Type = row.cells[1].textContent;
+            var Identification_Document = row.cells[2].textContent;
+            var First_Name = row.cells[4].textContent;
+            var Second_Name = row.cells[5].textContent;
+            var First_LastName = row.cells[6].textContent;
+            var Second_LastName = row.cells[7].textContent;
+            var Study_Name = row.cells[9].textContent;
+            var Date_Birth = row.cells[10].textContent;
+            var Email = row.cells[11].textContent;
+            var Phone_Number = row.cells[12].textContent;
+            var Gender = row.cells[13].textContent;
+            var Comment_Student = row.cells[14].textContent;
+            var Id_Student_Studies = row.cells[15].textContent;
+            var Id_Studies = row.cells[16].textContent;
+            var Book = row.cells[17].textContent;
+            var Folio = row.cells[18].textContent;
+            var Line = row.cells[19].textContent;
+            var Start_Date = row.cells[20].textContent;
+            var Termination_Date = row.cells[21].textContent;
+            var Comment_SS = row.cells[22].textContent;
+            var Date = row.cells[23].textContent;
+
+            document.getElementById('Id_Student').value = Id_Student;
+            document.getElementById('Document_Type').value = Document_Type;
+            document.getElementById('Identification_Document').value = Identification_Document;
+            document.getElementById('First_Name').value = First_Name;
+            document.getElementById('Second_Name').value = Second_Name;
+            document.getElementById('First_LastName').value = First_LastName;
+            document.getElementById('Second_LastName').value = Second_LastName;
+            document.getElementById('Study_Name').value = Study_Name;
+            document.getElementById('Date_Birth').value = Date_Birth;
+            document.getElementById('Email').value = Email;
+            document.getElementById('Phone_Number').value = Phone_Number;
+            document.getElementById('Gender').value = Gender;
+            document.getElementById('Comment_Student').value = Comment_Student;
+            document.getElementById('Id_Student_Studies').value = Id_Student_Studies;
+            document.getElementById('Id_Studies').value = Id_Studies;
+            document.getElementById('Book').value = Book;
+            document.getElementById('Folio').value = Folio;
+            document.getElementById('Line').value = Line;
+            document.getElementById('Start_Date').value = Start_Date;
+            document.getElementById('Termination_Date').value = Termination_Date;
+            document.getElementById('Comment_SS').value = Comment_SS;
+            document.getElementById('Date').value = Date;
+
+            document.querySelector('.save-changes-sd').style.display = 'none';
+            document.querySelector('.delete-btn-sd').style.display = 'none';
+
+            document.querySelector('.input-sd').style.display = 'inline-block';
+            document.querySelector('.select-sd').style.display = 'none';
+
+            document.querySelectorAll('#StudentForm input, #StudentForm select').forEach(field => {
+                field.disabled = true;
+            });
+        });
+    });
+    document.querySelectorAll('.edit-btn').forEach(button => {
+        // Handle edit button click 
+        button.addEventListener('click', function () {
+            var row = this.closest('tr');
+            var Id_Student = row.cells[0].textContent;
+            var Document_Type = row.cells[1].textContent;
+            var Identification_Document = row.cells[2].textContent;
+            var First_Name = row.cells[4].textContent;
+            var Second_Name = row.cells[5].textContent;
+            var First_LastName = row.cells[6].textContent;
+            var Second_LastName = row.cells[7].textContent;
+            var Study_Name = row.cells[9].textContent;
+            var Date_Birth = row.cells[10].textContent;
+            var Email = row.cells[11].textContent;
+            var Phone_Number = row.cells[12].textContent;
+            var Gender = row.cells[13].textContent;
+            var Comment_Student = row.cells[14].textContent;
+            var Id_Student_Studies = row.cells[15].textContent;
+            var Id_Studies = row.cells[16].textContent;
+            var Book = row.cells[17].textContent;
+            var Folio = row.cells[18].textContent;
+            var Line = row.cells[19].textContent;
+            var Start_Date = row.cells[20].textContent;
+            var Termination_Date = row.cells[21].textContent;
+            var Comment_SS = row.cells[22].textContent;
+            var Date = row.cells[23].textContent;
+
+            document.getElementById('Id_Student').value = Id_Student;
+            document.getElementById('Document_Type').value = Document_Type;
+            document.getElementById('Identification_Document').value = Identification_Document;
+            document.getElementById('First_Name').value = First_Name;
+            document.getElementById('Second_Name').value = Second_Name;
+            document.getElementById('First_LastName').value = First_LastName;
+            document.getElementById('Second_LastName').value = Second_LastName;
+            document.getElementById('Study_Name').value = Study_Name;
+            document.getElementById('Date_Birth').value = Date_Birth;
+            document.getElementById('Email').value = Email;
+            document.getElementById('Phone_Number').value = Phone_Number;
+            document.getElementById('Gender').value = Gender;
+            document.getElementById('Comment_Student').value = Comment_Student;
+            document.getElementById('Id_Student_Studies').value = Id_Student_Studies;
+            document.getElementById('Id_Studies').value = Id_Studies;
+            document.getElementById('Book').value = Book;
+            document.getElementById('Folio').value = Folio;
+            document.getElementById('Line').value = Line;
+            document.getElementById('Start_Date').value = Start_Date;
+            document.getElementById('Termination_Date').value = Termination_Date;
+            document.getElementById('Comment_SS').value = Comment_SS;
+            document.getElementById('Date').value = Date;
+
+            document.querySelector('.save-changes-sd').style.display = 'inline-block';
+            document.querySelector('.delete-btn-sd').style.display = 'inline-block';
+
+            document.querySelector('.input-sd').style.display = 'none';
+            document.querySelector('.select-sd').style.display = 'inline-block';
+
+
+            document.querySelectorAll('#StudentForm input, #StudentForm select').forEach(field => {
+                field.disabled = false;
+            });
+        });
+    });
+});
