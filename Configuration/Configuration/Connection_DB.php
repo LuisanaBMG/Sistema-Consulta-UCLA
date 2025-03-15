09de39ba175f8a1fe85ee867175015bb5f00617f
Luisana <163352714+LuisanaBMG@users.noreply.github.com>
2025-03-14 21:16:22 -0400

    Add files via upload

---
+++ Configuration/Configuration/Connection_DB.php
@@ -0,0 +1,11 @@
+<?php
+
+$Connection= mysqli_connect("localhost", "root", "", "ucla_system");
+
+/*if($Connection){
+    echo 'Conexión a la base de datos exitosa';
+}else{
+    echo 'Error al conectar con la base de datos';
+}*/
+
+?>
